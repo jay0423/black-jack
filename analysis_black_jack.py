17c0d969aabac395eded437cfd89728caa02c563
jay0423 <jay0423@i.softbank.jp>
2021-02-01 22:12:57 +0900

    プレイヤーとディーーラーがカードを引く処理の追加

--- analysis_black_jack.py
+++ analysis_black_jack.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 class MakeBlackJack:
 
+    card_list = []
     card_list_index = [] #デッキのカードリスト
     basic_strategy = pd.DataFrame() #ベーシックストラテジーの表
 
@@ -12,9 +13,12 @@ class MakeBlackJack:
     player_card = [] #プレイヤーのカード
 
     player_score = [0] #プレイヤーのスコア
+    bet_chip = [1]
 
     chip_list = []
     chip_over = 0
+
+    j_adj = 0
 
     def __init__(self):
         self.first_chip = 20
@@ -26,6 +30,7 @@ class MakeBlackJack:
         card_list.drop('card_num', axis=1, inplace=True)
         for i in range(deck):
             self.card_list_index += list(card_list.index)
+        self.card_list = card_list
     
     def import_basic_strategy(self):
         #ベーシックストラテジーの表をcsvファイルからインポート
@@ -57,8 +62,167 @@ class MakeBlackJack:
 
 
 
+    def make_player_score(self)
+        #プレイヤーの点数
+        self.player_score[self.j_adj] = 0
+        for i in range(len(self.player_card[self.j_adj])):
+            self.player_score[self.j_adj] += int(self.card_list.loc[self.player_card[self.j_adj][i]].num)
 
+
+    def decide_PC(self):
+        """
+        PC：player cardをベーシックストラテジ－の縦軸と同じ表記で出力する．
+        """
+        #表から行動を決める
+        player_card = self.player_card
+        j_adj = self.j_adj
+        card_list = self.card_list
+        #スプリット時の処理
+        if card_list.loc[player_card[j_adj][0]].num == card_list.loc[player_card[j_adj][1]].num \
+            and len(player_card[j_adj]) == 2 and player_card[j_adj][0][0] != 'A':
+            if str(player_card[j_adj]).count('A') == 2:
+                PC = 'AA'
+            else:
+                PC = str(card_list.loc[player_card[j_adj][0]].num) + str(card_list.loc[player_card[j_adj][1]].num)
+        #ソフトハンド
+        elif 'A' in str(player_card[j_adj]):
+            if len(player_card[j_adj]) == 2 and player_score == 11:
+                PC = '17:'
+            else:
+                player_card_check = player_card[j_adj].copy()
+                for card in player_card_check:
+                    if 'A' in card:
+                        player_card_check.remove(card)
+                        break
+                remainder = 0
+                for i in range(len(player_card_check)):
+                    remainder += card_list.loc[player_card_check[i]].num
+                if remainder <= 10:
+                    PC = 'A' + str(remainder)
+                    if PC =='A1': #スプリットした際に生じるバグの修正
+                        PC = 'A2'
+                elif 17 <= player_score[j_adj] <= 21:
+                    PC = '17:'
+                else:
+                    PC = player_score[j_adj]
+        #ハードハンド
+        else:
+            if player_score[j_adj] <= 8:
+                PC = ':8'
+            elif 17 <= player_score[j_adj] <= 21:
+                PC = '17:'
+            else:
+                PC = player_score[j_adj]
+        return PC
+
+    def select_HDPS_from_basic_strategy(self, PC, DC):
+        '''
+        ベーシックストラテジ－からHDPSの選択
+        入力 → 縦軸：PC (player card)，横軸：DC (dealer card)
+        出力 → H:ヒット, D:ダブルダウン, P:スプリット, S:スタンド
+        '''
+        return table.loc[str(PC), str(DC)]
     
+    def change_doubledown(self):
+        #ダブルダウン処理を無くす
+        self.basic_strategy.replace('D', 'H', inplace=True)
+        self.basic_strategy.loc['A7', 1:5] = 'S'
+
+    def H_action(self):
+        #P_actionがH（ヒット）となった際の処理．
+        self.player_card[self.j_adj].append(self.card_list_index[0])
+        self.card_list_index.pop(0)
+        self.change_doubledown()
+
+    def D_action(self):
+        #P_actionがD（ダブルダウン）となった際の処理．
+        self.bet_chip[self.j_adj] += self.bet_chip[self.j_adj]
+        self.player_card[self.j_adj].append(self.card_list_index[0])
+        self.player_score[self.j_adj] += int(self.card_list.loc[self.card_list_index[0]].num)
+        self.card_list_index.pop(0)
+
+    def P_action(self):
+        #P_actionがP（スプリット）となった際の処理．
+        self.bet_chip.append(self.bet_chip[self.j_adj])
+        self.player_card.insert(len(self.player_card), [self.player_card[self.j_adj][1]])
+        self.player_card[self.j_adj].pop(1)
+        self.player_score.append(0) #プレイヤーのスコアを分割
+        #Aでスプリットした場合にダブルダウンを無くす
+        if 'A' in self.player_card[self.j_adj][0]:
+            self.change_doubledown()
+
+    def player_draw(self):
+        #プレイヤ―がヒットし続けるまで処理を続ける．
+        while True:
+            self.make_player_score()
+
+            #プレイヤーがバストしたときの処理
+            if player_score[self.j_adj] >= 22:
+                break
+            
+            PC = self.decide_PC()
+            DC = self.card_list.loc[self.dealer_card[1]].num
+            #プレイヤーの行動を取得
+            P_action = self.select_HDPS_from_basic_strategy(PC, DC)
+            
+            #プレイヤーが行動を実行
+            if P_action == 'S':
+                break
+            elif P_action == 'H':
+                self.H_action()
+            elif P_action == 'D':
+                self.D_action()
+                break
+            elif P_action == 'P':
+                self.P_action()
+                break
+        #プレイヤーの最終得点
+        for i in range(len(player_score)):
+            if player_score[i] <= 11:
+                player_score[i] += 10
+            elif player_score[i] >= 22:
+                player_score[i] = 'BUST'
+
+
+    def dealer_draw(self):
+        """
+        ディーラーがカードを引く処理
+        """
+        #プレイヤーのBUSTを数値化
+        player_score_B = self.player_score.copy()
+        for score in self.player_score:
+            if score == 'BUST':
+                player_score_B[i] = 0
+        #ディーラーがカードを引く
+        while True:
+            #ディーラーのスコア
+            dealer_score = 0
+            for card in self.dealer_card:
+                dealer_score += self.card_list.loc[card].num
+            #Aが含まれている場合の処理
+            if 'A' in str(dealer_card):
+                if 18 <= dealer_score + 10 <= 21:
+                    break
+                elif 18 <= dealer_score <= 21:
+                    break
+                elif dealer_score >= 22:
+                    break
+                elif dealer_score + 10 == 17 and player_score_B[self.j_adj] <= 16:
+                    break
+            else:
+                if dealer_score >= 17:
+                    break
+            #ヒット
+            dealer_card.append(card_list_index[0])
+            card_list_index.pop(0)
+        #ディーラーの最終得点
+        if dealer_score <= 11:
+            dealer_score += 10
+        elif dealer_score >= 22:
+            dealer_score = 'BUST'
+
+        
+
 
     def main(self):
         #前処理
@@ -68,3 +232,7 @@ class MakeBlackJack:
         self.get_dealer_card()
         self.get_player_card()
         
+        #プレイヤーがカードを引く処理
+        self.player_draw()
+        #ディーラーがカードを引く処理
+        self.dealer_draw()
