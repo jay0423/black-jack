78bad3206790785e5ef95277602a1cd06d9b51fb
jay0423 <jay0423@i.softbank.jp>
2021-02-01 18:18:01 +0900

    MakeBlackJackクラスを作成し，csvファイルからデータを取るメソッドを作成

--- analysis_black_jack.py
+++ analysis_black_jack.py
@@ -3,5 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+class MakeBlackJack:
+
+    card_list_index = []
+    basic_strategy = pd.DataFrame()
+    chip_list = []
+    chip_over = 0
+
+    def __init__(self):
+        self.first_chip = 20
+
+    def import_cards(self):
+        #AからKのカードのリストをインポート
+        card_list = pd.read_csv('playing_card.csv')
+        card_list.index = card_list.card_num
+        card_list.drop('card_num', axis=1, inplace=True)
+        for i in range(deck):
+            self.card_list_index += list(card_list.index)
+    
+    def import_basic_strategy(self):
+        #ベーシックストラテジーの表をcsvファイルからインポート
+        self.basic_strategy = pd.read_csv('BlackJack_table{}.csv'.format(table_choice))
+        self.basic_strategy.index = self.basic_strategy.PC
+        self.basic_strategy.drop('PC', axis=1, inplace=True)
+
+    
 
 
