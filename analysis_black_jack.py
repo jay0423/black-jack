8c9ac91bfca6758b04f31d5411603cdefc0b3b7d
jay0423 <jay0423@i.softbank.jp>
2021-02-01 19:15:37 +0900

    カードを引くメソッド，ナチュラルブラックジャックの確認メソッドを追加．

--- analysis_black_jack.py
+++ analysis_black_jack.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 
 class MakeBlackJack:
 
-    card_list_index = []
-    basic_strategy = pd.DataFrame()
+    card_list_index = [] #デッキのカードリスト
+    basic_strategy = pd.DataFrame() #ベーシックストラテジーの表
+
+    dealer_card = [] #ディーラーのカード
+    player_card = [] #プレイヤーのカード
+
+    player_score = [0] #プレイヤーのスコア
+
     chip_list = []
     chip_over = 0
 
@@ -27,6 +33,38 @@ class MakeBlackJack:
         self.basic_strategy.index = self.basic_strategy.PC
         self.basic_strategy.drop('PC', axis=1, inplace=True)
 
+    def shuffle_card(self, card):
+        #カードデッキをシャッフルする
+        return np.random.shuffle(card)
+
+    def get_dealer_card(self):
+        #ディーラーがカードを引く
+        self.dealer_card = [card_list_index[0], card_list_index[2]]
+        del self.card_list_index[:2] #引いた分のカードを削除
+
+    def get_player_card(self):
+        #プレイヤーがカードを引く
+        self.player_card = [[card_list_index[0], card_list_index[2]]]
+        del self.card_list_index[:2] #引いた分のカードを削除
+    
+    def check_natural_black_jack(self):
+        #ディーラーのナチュラルブラックジャック確認
+        dealer_score_start = 0
+        for i in range(2):
+            dealer_score_start += int(card_list.loc[self.dealer_card[i]].num)
+        if dealer_score_start == 11 and 'A' in str(self.dealer_card):
+            self.basic_strategy.replace(['H', 'D', 'P'], 'S', inplace=True)
+
+
+
+
     
 
-
+    def main(self):
+        #前処理
+        self.import_cards()
+        self.import_basic_strategy()
+        self.shuffle_card()
+        self.get_dealer_card()
+        self.get_player_card()
+        
