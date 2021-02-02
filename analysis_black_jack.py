cc2b3cc5c1b45cce3b38f4542bb618755c0f48f8
jay0423 <jay0423@i.softbank.jp>
2021-02-03 00:12:56 +0900

    dfにスプリットの回数とコインの収益の結果の列を追加．

--- analysis_black_jack.py
+++ analysis_black_jack.py
@@ -30,6 +30,27 @@ class Analysis:
     def make_df(self, dicts):
         return pd.DataFrame(dicts)
 
+    def edit_df(self, df):
+        df["split"] = df["player_card"].map(len) - 1
+
+        def func2(row):
+            def func(xi):
+                ai = []
+                for x in xi:
+                    if x == "WIN":
+                        ai.append(1)
+                    elif x == "LOSE":
+                        ai.append(-1)
+                    elif x == "Black Jack":
+                        ai.append(1.5)
+                    elif x == "PUSH":
+                        ai.append(0)
+                return ai
+            return np.dot(func(row["player_WL"]), row["bet_chip"])
+
+        df["get_coin"] = df.apply(func2, axis=1)
+        return df
+
     def main(self):
         self.a.setup()
         for i in range(self.GAME_TIME):
@@ -43,4 +64,5 @@ class Analysis:
             "bet_chip": self.bet_chip
         }
         df = self.make_df(dicts)
+        df = self.edit_df(df)
         return df
